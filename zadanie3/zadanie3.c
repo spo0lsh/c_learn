94740cb3e98628e8b1e344a727ec67aec31eec46
scorp <scorp@svn.(none)>
2013-06-17 13:19:27 +0200

    added structs and unions - first version

--- zadanie3/zadanie3.c
+++ zadanie3/zadanie3.c
@@ -18,5 +18,33 @@ Funkcję, która odczytuje wszystkie wiadomości z buforu w kolejności
 */
 
 int main() {
+	// definicja buffora
+	char * buff = malloc(sizeof(msg1_t) +  sizeof(msg2_t) + sizeof(msg3_t));
+	if(buff == NULL) {
+		printf("Something wrong!\n");
+		free(buff);
+		buff=NULL;
+		exit(EXIT_FAILURE);
+	}
+	
+	// debug
+	//test_size();
+	
+	// zwalnianie buffora
+	free(buff);
+	buff=NULL;
 	return(EXIT_SUCCESS);
+}
+
+
+void test_size() {
+	printf("int %d\n", sizeof(int));
+	printf("short int %d\n", sizeof(short int));
+	printf("char %d\n", sizeof(char));
+	printf("long int %d\n", sizeof(long int));
+	printf("float %d\n", sizeof(float));
+	printf("double %d\n", sizeof(double));
+	printf("msg1_t %d\n", sizeof(msg1_t));
+	printf("msg2_t %d\n", sizeof(msg2_t));
+	printf("msg3_t %d\n", sizeof(msg3_t));
 }
--- zadanie3/zadanie3.h
+++ zadanie3/zadanie3.h
@@ -2,4 +2,33 @@
 #define __zadanie3_H
 
 
+void test_size(); // testowa
+
+// kombinacje (enum)
+typedef enum comb_e {
+	COMB1, COMB2, COMB3
+}comb_t;
+// definicja wiadomosci
+typedef struct msg1_s {
+	short int m1;
+	short int m2;
+	short int m3;
+} __attribute__((packed)) msg1_t;
+typedef struct msg2_s {
+	float m1;
+	float m2;
+	float m3;
+} __attribute__((packed)) msg2_t;
+typedef struct msg3_s {
+	double m1;
+	double m2;
+	double m3;
+} __attribute__((packed)) msg3_t;
+
+typedef union {
+	msg1_t um1;
+	msg2_t um2;
+	msg3_t um3;
+} msg_t;
+
 #endif /* __zadanie3_H */
