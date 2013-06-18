1d59c277d33969a52228a9109545a5cc6eca82ff
scorp <scorp@svn.(none)>
2013-06-18 13:38:35 +0200

    next lession: hashtable

---
+++ hashtable1/hashtable.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hashtable.h"
+#include "uthash.h"
+
+// EXIT_FAILURE
+// EXIT_SUCCESS
+int main() {
+	
+	//
+
+	//
+	
+	return(EXIT_SUCCESS);
+}
---
+++ hashtable1/hashtable.h
@@ -0,0 +1,5 @@
+#ifndef __hashtable_H
+#define __hashtable_H
+
+
+#endif /* __hashtable_H */
