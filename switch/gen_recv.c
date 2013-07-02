62e54950764c666b4ea4ed944b0fcd54e196bdfb
scorp <scorp@svn.(none)>
2013-07-02 13:43:19 +0200

    work tx/rx MQ

---
+++ switch/gen_recv.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "gen_recv.h"
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <memory.h>
+//#define DEBUG
+
+int main() {
+	int msqid;
+    key_t key;
+    message_buf  rbuf;
+	SFrame frame;
+
+    /*
+     * Get the message queue id for the
+     * "name" 1234, which was created by
+     * the server.
+     */
+    key = 1234;
+
+    if ((msqid = msgget(key, 0666)) < 0) {
+        perror("msgget");
+        exit(1);
+    }
+
+    /*
+     * Receive an answer of message type 1.
+     */
+    if (msgrcv(msqid, &rbuf, sizeof(rbuf.frame), 1, 0) < 0) {
+        perror("msgrcv");
+        exit(1);
+    }
+	frame = rbuf.frame;
+	
+    /*
+     * Print the answer.
+     */
+	printf("%02x:%02x:%02x:%02x:%02x:%02x\n", frame.ach_MACdst[0], frame.ach_MACdst[1], frame.ach_MACdst[2], frame.ach_MACdst[3], frame.ach_MACdst[4], frame.ach_MACdst[5]);
+	printf("%02x:%02x:%02x:%02x:%02x:%02x\n", frame.ach_MACsrc[0], frame.ach_MACsrc[1], frame.ach_MACsrc[2], frame.ach_MACsrc[3], frame.ach_MACsrc[4], frame.ach_MACsrc[5]);
+	printf("Length: %d\n", frame.ach_Length);
+	printf("Payload: %s\n", frame.ach_Payload);
+	printf("CRC: %s\n", frame.ach_crc);
+
+
+    return(EXIT_SUCCESS);
+}
---
+++ switch/gen_send.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "gen_send.h"
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <memory.h>
+//#define DEBUG
+
+int main() {
+	
+	//tworzenie
+	SFrame frame;
+	frame.ach_MACdst[0] = 0x00;
+	frame.ach_MACdst[1] = 0x0d;
+    frame.ach_MACdst[2] = 0x3f;
+    frame.ach_MACdst[3] = 0xff;
+    frame.ach_MACdst[4] = 0x02;
+    frame.ach_MACdst[5] = 0x5f;
+	
+	frame.ach_MACsrc[0] = 0x00;
+	frame.ach_MACsrc[1] = 0x0d;
+	frame.ach_MACsrc[2] = 0x3f;
+	frame.ach_MACsrc[3] = 0xff;
+	frame.ach_MACsrc[4] = 0xff;
+	frame.ach_MACsrc[5] = 0xff;
+
+	frame.ach_Length = 64;
+	(void) strcpy(frame.ach_Payload,"DUAP");
+	(void) strcpy(frame.ach_crc,"42");
+	
+	// 
+	int msqid;
+	int msgflg = IPC_CREAT | 0666;
+	key_t key;
+	message_buf sbuf;
+	key = 1234;
+	
+	//(void) fprintf(stderr, "\nmsgget: Calling msgget(%#lx,\%#o)\n",key, msgflg);
+    if ((msqid = msgget(key, msgflg )) < 0) {
+        perror("msgget");
+        exit(1);
+    }
+    else 
+		(void) fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
+	
+	sbuf.mtype = 1;
+	(void) fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
+	sbuf.frame = frame;
+	printf("Frame Length: %d\n", sbuf.frame.ach_Length);
+	(void) fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
+	
+	if (msgsnd(msqid, &sbuf, sizeof(sbuf.frame), IPC_NOWAIT) < 0) {
+		//printf("%d %"l %l \n", msqid, sbuf.mtype, sizeof(sbuf.frame) );
+		perror("msgsnd");
+		exit(1);
+	} else 
+		printf("Message: Sent\n");
+	
+    return(EXIT_SUCCESS);
+}
