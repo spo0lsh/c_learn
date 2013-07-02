6e8334c551ee60dea5041d56e0437b309107c21c
scorp <scorp@svn.(none)>
2013-07-02 14:01:25 +0200

    pointers

--- switch/gen_recv.c
+++ switch/gen_recv.c
@@ -12,15 +12,16 @@ int main() {
 	int msqid;
     key_t key;
     message_buf  rbuf;
-	SFrame frame;
+	SFrame *frame;
 
     /*
      * Get the message queue id for the
      * "name" 1234, which was created by
      * the server.
      */
-    key = 1234;
-
+    //key = 1234;
+    key = ftok("/tmp/bridge0", 'b');
+	
     if ((msqid = msgget(key, 0666)) < 0) {
         perror("msgget");
         exit(1);
@@ -33,16 +34,16 @@ int main() {
         perror("msgrcv");
         exit(1);
     }
-	frame = rbuf.frame;
+	frame = &rbuf.frame;
 	
     /*
      * Print the answer.
      */
-	printf("%02x:%02x:%02x:%02x:%02x:%02x\n", frame.ach_MACdst[0], frame.ach_MACdst[1], frame.ach_MACdst[2], frame.ach_MACdst[3], frame.ach_MACdst[4], frame.ach_MACdst[5]);
-	printf("%02x:%02x:%02x:%02x:%02x:%02x\n", frame.ach_MACsrc[0], frame.ach_MACsrc[1], frame.ach_MACsrc[2], frame.ach_MACsrc[3], frame.ach_MACsrc[4], frame.ach_MACsrc[5]);
-	printf("Length: %d\n", frame.ach_Length);
-	printf("Payload: %s\n", frame.ach_Payload);
-	printf("CRC: %s\n", frame.ach_crc);
+	printf("%02x:%02x:%02x:%02x:%02x:%02x\n", frame->ach_MACdst[0], frame->ach_MACdst[1], frame->ach_MACdst[2], frame->ach_MACdst[3], frame->ach_MACdst[4], frame->ach_MACdst[5]);
+	printf("%02x:%02x:%02x:%02x:%02x:%02x\n", frame->ach_MACsrc[0], frame->ach_MACsrc[1], frame->ach_MACsrc[2], frame->ach_MACsrc[3], frame->ach_MACsrc[4], frame->ach_MACsrc[5]);
+	printf("Length: %d\n", frame->ach_Length);
+	printf("Payload: %s\n", frame->ach_Payload);
+	printf("CRC: %s\n", frame->ach_crc);
 
 
     return(EXIT_SUCCESS);
