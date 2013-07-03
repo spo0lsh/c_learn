2f186a188722ebc9387ecabfb023f750e26c6798
scorp <scorp@svn.(none)>
2013-07-03 10:38:33 +0200

    workaround but works

--- switch/send.c
+++ switch/send.c
@@ -7,7 +7,50 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
+
+
 void fn_send(int i, SFrame *frame) {
+
+	SFrame frame1;
+	memcpy(&frame1, frame, sizeof(message_buf));
+	printf("[S2] Length: %d\n", frame->ach_Length);
+	
+	// 
+	int msqid;
+	int msgflg = IPC_CREAT | 0666;
+	key_t key;
+	message_buf sbuf;
+	key = 1100 + i;
+	//key = ftok("/tmp/bridge0", 'b');
+	
+	//(void) fprintf(stderr, "\nmsgget: Calling msgget(%#lx,\%#o)\n",key, msgflg);
+    if ((msqid = msgget(key, msgflg )) < 0) {
+        perror("msgget");
+        exit(1);
+    }
+    else 
+		(void) fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
+	
+	sbuf.mtype = 1;
+	(void) fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
+	sbuf.frame = frame1;
+	//memcpy(&sbuf.frame, frame, sizeof(message_buf));
+	//memcpy((void *)sbuf.frame, (void *) &frame, sizeof(message_buf));
+	printf("frame1 Length: %d\n", sbuf.frame.ach_Length);
+	(void) fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
+	
+	if (msgsnd(msqid, &sbuf, sizeof(sbuf.frame), IPC_NOWAIT) < 0) {
+		//printf("%d %"l %l \n", msqid, sbuf.mtype, sizeof(sbuf.frame1) );
+		perror("msgsnd");
+		exit(1);
+	} else 
+		printf("Message: Sent\n");
+}
+
+
+
+
+	/*
 	int msqid;
 	int msgflg = IPC_CREAT | 0666;
 	key_t key;
@@ -39,4 +82,5 @@ void fn_send(int i, SFrame *frame) {
 		exit(1);
 	} else 
 		printf("Message: Sent\n");
-}
+	*/
+	
