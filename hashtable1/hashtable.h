d038252c91effbd25b2651cdd20cadf76b1ac5d2
scorp <scorp@svn.(none)>
2013-07-05 09:50:14 +0200

    dont know

--- hashtable1/hashtable.h
+++ hashtable1/hashtable.h
@@ -3,3 +3,14 @@
 
 
 #endif /* __hashtable_H */
+
+
+/*
+http://epaperpress.com/sortsearch/has.html 
+http://www.partow.net/programming/hashfunctions/ http://algs4.cs.princeton.edu/34hash/ 
+http://en.wikibooks.org/wiki/Data_Structures/Hash_Tables 
+http://algorithms.openmymind.net/structures/hashtables.html 
+http://www.cs.auckland.ac.nz/software/AlgAnim/hash_tables.html 
+http://www.sparknotes.com/cs/searching/hashtables/section3.rhtml 
+http://techforum4u.com/content.php/383-How-To-Implement-Hash-Table-In-C
+*/
