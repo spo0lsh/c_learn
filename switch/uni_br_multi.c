22ca0b27988f7cd120a140ff74c2322d2b00929a
scorp <scorp@svn.(none)>
2013-07-05 14:40:16 +0200

    improve br/mc/uc check

--- switch/uni_br_multi.c
+++ switch/uni_br_multi.c
@@ -8,6 +8,14 @@ int fn_unicast_broadcast_multicast(int bridgeport, SFrame *frame) {
 	unsigned char mac_br[6]  = {0xff,0xff,0xff,0xff,0xff,0xff};
 	unsigned char mac_mc1[6] = {0x01,0x00,0x5e,0x00,0x00,0x00};
 	unsigned char mac_mc2[6] = {0x01,0x00,0x5e,0x7f,0xff,0xff};
+	/*
+	frame->ach_MACdst[0] = 0xff;
+	frame->ach_MACdst[1] = 0xff;
+	frame->ach_MACdst[2] = 0xff;
+	frame->ach_MACdst[3] = 0xff;
+	frame->ach_MACdst[4] = 0xff;
+	frame->ach_MACdst[5] = 0xff;
+	*/
 	/* something to check MAC addresses */
 	#ifdef DEBUG
 	printf("fn_unicast_broadcast_multicast %d\n",bridgeport);
@@ -16,23 +24,17 @@ int fn_unicast_broadcast_multicast(int bridgeport, SFrame *frame) {
 	printf("[U_BR_MC] Length: %d\n", frame->ach_Length);
 	printf("[U_BR_MC] Payload: %s\n", frame->ach_Payload);
 	printf("[U_BR_MC] CRC: %s\n", frame->ach_crc);
+	//printf("%d\n",frame->ach_MACdst[0] % 2);
 	#endif
-	
-	if(memcmp(frame->ach_MACdst, mac_br,6) == 0) {
+	if((frame->ach_MACdst[0] % 2 ) == 1 ) {
 		#ifdef DEBUG
-		printf("[U_BR_MC] broadcast\n");
+		printf("[U_BR_MC] broadcast or multicast\n");
 		#endif
-		return(1);
-	}
-	else if(memcmp(frame->ach_MACdst, mac_mc1,6) >= 0 && memcmp(frame->ach_MACdst, mac_mc2,6) <= 0) {
-		#ifdef DEBUG
-		printf("[U_BR_MC] multicast\n");
-		#endif
-		return(1);
+		return 1;
 	} else {
 		#ifdef DEBUG
 		printf("[U_BR_MC] unicast\n");
 		#endif
-		return(0);
+		return 0;
 	}
 }
