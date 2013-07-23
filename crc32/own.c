940c58e36fcbc7f7db98d99ed30ef0b187af2058
scorp <scorp@svn.(none)>
2013-07-23 15:16:41 +0200

    almost work

--- crc32/own.c
+++ crc32/own.c
@@ -84,7 +84,29 @@ int main() {
 	printf("The crcSlow() of \"123456789\" is 0xCBF43926\n");
 	
 	printf("================\n");
+	/*
 	char text[] = "123456789";
+	int j;
+	unsigned int byte, crc;
+	i = 0;
+	crc = 0xFFFFFFFF;
+	while(text[i] != 0) {
+		byte = text[i];            // Get next byte.
+		byte = reverse(byte);         // 32-bit reversal.
+		for (j = 0; j <= 7; j++) {    // Do eight times.
+			if ((int)(crc ^ byte) < 0) {
+				crc = (crc << 1) ^ 0x04C11DB7;
+			} else {
+				crc = crc << 1;
+			}
+			byte = byte << 1;          // Ready next msg bit.
+		}
+		i = i + 1;
+	}
+	*/
+	char *text;
+	text = "123456789";
+	
 	int j;
 	unsigned int byte, crc;
 	i = 0;
