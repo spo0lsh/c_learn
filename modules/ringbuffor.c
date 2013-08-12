4de5a1085578d31c29bad077783c25114c03ce29
scorp <scorp@devel.(none)>
2013-08-12 12:00:03 +0200

    improve char create destroy and remove warring

--- modules/ringbuffor.c
+++ modules/ringbuffor.c
@@ -1,57 +1,72 @@
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/version.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
 #include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 #include <asm/uaccess.h>
-
 #define MY_MACIG 'G'
 #define READ_IOCTL _IOR(MY_MACIG, 0, int)
 #define WRITE_IOCTL _IOW(MY_MACIG, 1, int)
- 
-static int major; 
-static char msg[200];
+
+static char c;
+static dev_t first; // Global variable for the first device number 
+static struct cdev c_dev; // Global variable for the character device structure
+static struct class *cl; // Global variable for the device class
+static int my_open(struct inode *i, struct file *f)
+{
+  printk(KERN_INFO "Driver: open()\n");
+  return 0;
+}
+  static int my_close(struct inode *i, struct file *f)
+{
+  printk(KERN_INFO "Driver: close()\n");
+  return 0;
+}
+
+static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
+{
+    printk(KERN_INFO "Driver: read()\n");
+    if (*off == 0)
+    {
+        if (copy_to_user(buf, &c, 1) != 0)
+            return -EFAULT;
+        else
+        {
+            (*off)++;
+            return 1;
+        }
+    }
+    else
+        return 0;
+}
+
+  static ssize_t my_write(struct file *f, const char __user *buf,
+  size_t len, loff_t *off)
+{
+  printk(KERN_INFO "Driver: write()\n");
+    if(copy_from_user(&c, buf + len - 1,1) != 0)
+        return -EFAULT;
+    else
+        return len;
+  return len;
+}
+
 char buf[200];
-
-typedef struct {
-    int size;   /* maximum number of elements           */
-    int start;  /* index of oldest element              */
-    int count;    /* index at which to write new element  */
-    char msg[200];  /* elements  */
-} CircularBuffer;
-
-
-static ssize_t device_read(struct file *filp, char __user *buffer, size_t length, loff_t *offset)
+int my_ioctl(struct inode *inode, struct file *f, unsigned int cmd, unsigned long arg)
 {
-  	return simple_read_from_buffer(buffer, length, offset, msg, 200); // read from kernel
-}
-
-
-static ssize_t device_write(struct file *filp, const char __user *buff, size_t len, loff_t *off)
-{
-	if (len > 199)
-		return -EINVAL;
-	//copy_from_user(msg, buff, len); // write to kernel
-	if(copy_from_user(msg, buff, len) != 0) {
-		return -EFAULT;
-	} 
-	else 
-	{
-		msg[len] = '\0';
-		return len;
-	}
-}
-
-int device_ioctl(struct inode *inode, struct file *filep, unsigned int cmd, unsigned long arg) {
 	int len = 200;
 	switch(cmd) {
 	case READ_IOCTL:	
-		//copy_to_user((char *)arg, buf, 200);
 		if(copy_to_user((char *)arg, buf, 200) != 0) {
-			printk(KERN_INFO "ioctl read: something wrong\n");
+			return -EFAULT;
 		}
 		break;
 	
 	case WRITE_IOCTL:
-		if(copy_from_user(buf, (char *)arg, len) != 0) {
+		if(copy_from_user(buf, (char *)arg, len) !=0) {
 			printk(KERN_INFO "ioctl write: something wrong\n");
 		}
 		break;
@@ -60,30 +75,58 @@ int device_ioctl(struct inode *inode, struct file *filep, unsigned int cmd, unsi
 		return -ENOTTY;
 	}
 	return len;
-
 }
-static struct file_operations fops = {
-	.read = device_read, 
-	.write = device_write,
-	.ioctl = device_ioctl,
+
+
+  static struct file_operations pugs_fops =
+{
+  .owner = THIS_MODULE,
+  .open = my_open,
+  .release = my_close,
+  .read = my_read,
+  .write = my_write,
+  .ioctl = my_ioctl
 };
-
-static int __init cdevexample_module_init(void)
+ 
+static int __init ofcd_init(void) /* Constructor */
 {
-	major = register_chrdev(0, "ringbuffor", &fops);
-	if (major < 0) {
-     		printk ("Registering the character device failed with %d\n", major);
-	     	return major;
-	}
-	printk("cdev example: assigned major: %d\n", major);
-	printk("create node with mknod /dev/ringbuffor c %d 0\n", major);
- 	return 0;
+  printk(KERN_INFO "Namaskar: ofcd registered");
+  if (alloc_chrdev_region(&first, 0, 1, "Shweta") < 0)
+  {
+    return -1;
+  }
+    if ((cl = class_create(THIS_MODULE, "chardrv")) == NULL)
+  {
+    unregister_chrdev_region(first, 1);
+    return -1;
+  }
+    if (device_create(cl, NULL, first, NULL, "ringbuffor") == NULL)
+  {
+    class_destroy(cl);
+    unregister_chrdev_region(first, 1);
+    return -1;
+  }
+    cdev_init(&c_dev, &pugs_fops);
+    if (cdev_add(&c_dev, first, 1) == -1)
+  {
+    device_destroy(cl, first);
+    class_destroy(cl);
+    unregister_chrdev_region(first, 1);
+    return -1;
+  }
+  return 0;
 }
-
-static void __exit cdevexample_module_exit(void)
+ 
+static void __exit ofcd_exit(void) /* Destructor */
 {
-	unregister_chrdev(major, "ringbuffor");
-}  
+  cdev_del(&c_dev);
+  device_destroy(cl, first);
+  class_destroy(cl);
+  unregister_chrdev_region(first, 1);
+  printk(KERN_INFO "Alvida: ofcd unregistered");
+}
+ 
+module_init(ofcd_init);
+module_exit(ofcd_exit);
+MODULE_LICENSE("GPL");
 
-module_init(cdevexample_module_init);
-module_exit(cdevexample_module_exit);
