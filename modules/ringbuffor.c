aac2b8f8956baf6a2274c919483159ff6b119716
scorp <scorp@devel.(none)>
2013-08-12 12:28:24 +0200

    with debuging, before write to RB

--- modules/ringbuffor.c
+++ modules/ringbuffor.c
@@ -7,14 +7,80 @@
 #include <linux/device.h>
 #include <linux/cdev.h>
 #include <asm/uaccess.h>
+#define BUFFER_SIZE 3
 #define MY_MACIG 'G'
 #define READ_IOCTL _IOR(MY_MACIG, 0, int)
 #define WRITE_IOCTL _IOW(MY_MACIG, 1, int)
+#define DEBUG
 
+typedef struct {
+	int size;   /* maximum number of elements           */
+	int start;  /* index of oldest element              */
+	int count;    /* index at which to write new element  */
+	char array[BUFFER_SIZE];  /* vector of elements                   */
+} CircularBuffer;
+
+static CircularBuffer cb; // global variable for CB
 static char c;
 static dev_t first; // Global variable for the first device number 
 static struct cdev c_dev; // Global variable for the character device structure
 static struct class *cl; // Global variable for the device class
+
+
+
+void cbInit(CircularBuffer *cb)
+{
+	int i;
+	cb->size  = BUFFER_SIZE;
+	cb->start = 0;
+	cb->count = 0;
+	for(i=0;i<BUFFER_SIZE;++i)
+	{
+		cb->array[i]=0;
+	}
+}
+
+int cbIsFull(CircularBuffer *cb)
+{
+	return cb->count == cb->size;
+}
+ 
+int cbIsEmpty(CircularBuffer *cb)
+{
+	return cb->count == 0;
+}
+
+void cbWrite(CircularBuffer *cb,  char value)
+{
+	#ifdef DEBUG
+	printk(KERN_INFO "[W] %d %0x\n", value,value);
+	#endif
+	int end=(cb->start + cb->count) % cb->size;
+    cb->array[end]=value;
+    if (cb->count == cb->size)
+    {
+        cb->start=(cb->start + 1) % cb->size;
+	}
+    else
+    {
+        ++cb->count; 
+	}
+	#ifdef DEBUG
+	printk(KERN_INFO "[W] start %d count %d\n",cb->start,cb->count);
+	#endif
+}
+ 
+void cbRead(CircularBuffer *cb,  char  *value)
+{
+    *value=cb->array[cb->start];
+    #ifdef DEBUG
+    printk(KERN_INFO "[R] %d %0x\n", ( char )*value,( char )*value);
+    #endif
+    cb->start=(cb->start + 1) % cb->size;
+    --cb->count;
+}
+
+
 static int my_open(struct inode *i, struct file *f)
 {
   printk(KERN_INFO "Driver: open()\n");
@@ -40,17 +106,30 @@ static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off
         }
     }
     else
+    {
         return 0;
+	}
 }
 
-  static ssize_t my_write(struct file *f, const char __user *buf,
-  size_t len, loff_t *off)
+static ssize_t my_write(struct file *f, const char __user *buf, size_t len, loff_t *off)
 {
   printk(KERN_INFO "Driver: write()\n");
+    //if(copy_from_user(&c, buf + len - 1,1) != 0)
+    //printk(KERN_INFO "buf = %s clen = %d\n",len);
+    int i;
+    for(i=0;i<len;++i)
+    {
+		printk(KERN_INFO "[%d]buf = %c clen = %d\n",i+1,(char)buf[i],len);
+	}
     if(copy_from_user(&c, buf + len - 1,1) != 0)
+    //cbWrite(&cb, (unsigned int)value)
+    {
         return -EFAULT;
+	}
     else
+    {
         return len;
+	}
   return len;
 }
 
@@ -114,6 +193,8 @@ static int __init ofcd_init(void) /* Constructor */
     unregister_chrdev_region(first, 1);
     return -1;
   }
+  cbInit(&cb);
+  
   return 0;
 }
  
