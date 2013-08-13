a52fe42f58716185fbaca923fc2e23cd8e2e91d8
scorp <scorp@devel.(none)>
2013-08-13 14:44:14 +0200

    improve ioctl user

--- modules/ioctl_user.c
+++ modules/ioctl_user.c
@@ -11,22 +11,41 @@
 #define SIZE_IOCTL _IO(MY_MACIG, 5) // defines our ioctl call
 
 int main(){
-	char buf[200];
+	char buf[256];
 	int fd = -1;
 	if ((fd = open("/dev/ringbuffor", O_RDWR)) < 0) {
 		perror("open");
 		return -1;
 	}
 	if(ioctl(fd, WRITE_IOCTL, "hello world") < 0)
-		perror("first ioctl");
+		perror("WRITE ioctl");
 	if(ioctl(fd, READ_IOCTL, buf) < 0)
-		perror("second ioctl");
+		perror("READ ioctl");
 	if(ioctl(fd, EMPTY_IOCTL) < 0) {
 		printf("Empty\n");
 	}
+	if(ioctl(fd, WRITE_IOCTL, "hello world") < 0)
+		perror("WRITE ioctl");
 	if(ioctl(fd, FULL_IOCTL) < 0) {
 		printf("Full\n");
 	}
+	if(ioctl(fd, SIZE_IOCTL) < 0) {
+		printf("Size: %d\n",ioctl(fd, SIZE_IOCTL));
+	} else {
+		printf("Size: %d\n",ioctl(fd, SIZE_IOCTL));
+	}
+	if(ioctl(fd, CLEAR_IOCTL) < 0) {
+		perror("Clear\n");
+	}
+	if(ioctl(fd, EMPTY_IOCTL) < 0) {
+		printf("Empty\n");
+	}
+	if(ioctl(fd, SIZE_IOCTL) < 0) {
+		printf("Size: %d\n",ioctl(fd, SIZE_IOCTL));
+	} else {
+		printf("Size: %d\n",ioctl(fd, SIZE_IOCTL));
+	}
+	printf("\n");
 	printf("message: %s\n", buf);
 	return 0;
 
