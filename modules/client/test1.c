1eba0f7f1ba7c3eafdcef61bbec7ac83fa86e959
scorp <scorp@devel.(none)>
2013-08-21 15:07:12 +0200

    something

--- modules/client/test1.c
+++ modules/client/test1.c
@@ -6,6 +6,44 @@
 
 int main()
 {
+	const char*	directory = "/proc";
+	size_t		taskNameSize = 1024; 
+	char*		taskName = calloc(1, taskNameSize); // ?!
+	char*		name="bash"; // what we should found
+	
+	DIR*		dir = opendir(directory);
+	
+	
+	if(dir) {
+		
+		struct dirent* de = 0; //?
+		
+		while ((de = readdir(dir)) != 0)
+		{
+			if (strcmp(de->d_name, ".") == 0 || strcmp(de->d_name, "..") == 0)
+			{
+				continue;
+			}
+			int pid = -1;
+			int res = sscanf(de->d_name, "%d", &pid);
+			if (res == 1)
+			{
+				char cmdline_file[1024] = {0};
+				sprintf(cmdline_file, "%s/%d/cmdline", directory, pid);
+				FILE* cmdline = fopen(cmdline_file, "r"); // open file (from sprintf)
+				if (getline(&taskName, &taskNameSize, cmdline) > 0)
+				{
+					if (strstr(taskName, name) != 0)
+					{
+						fprintf(stdout, "A %s process, with PID %d, has been detected.\n", name, pid);
+					}
+				}
+				fclose(cmdline);
+			}
+		}
+		
+		closedir(dir);
+	}
 	
 	return 0;
 }
